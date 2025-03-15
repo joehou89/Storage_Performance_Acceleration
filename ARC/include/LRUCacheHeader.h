19e08e058aaa4e7727589acf0936b9bfdec7f51f
h18848 <hou.chao@h3c.com>
2025-03-16 00:23:45 +0800

    PN:修改文件

--- ARC/include/LRUCacheHeader.h
+++ ARC/include/LRUCacheHeader.h
@@ -34,35 +34,24 @@ public:
         transform_time = transform_time;
     }
     
-    bool put(DataType data); // 输入元素
-    void Add(DataType data); // 输入并且扩充一格
-    bool Subtract();         // 删除末尾元素且空间缩减，返回是否成功
-    // 其中，由于只是插入元素，则对于ghost的处理放在插入元素函数中即可
-    bool check_ghost(DataType data); // 检查ghost中是否有对应元素,有则返回true
-    void Show(bool show_ghost); // 显示所有元素
+    bool cache_insert(DataType data);                       // 插入元素
+    void cache_insert_and_extend(DataType data);            // 插入并且扩充一格
+    bool cache_evict_and_subtract();                        // 删除末尾元素且空间缩减，返回是否成功
+    bool check_ghost(DataType data);                        // 检查ghost中是否有对应元素,有则返回true
+    void cache_show(bool show_ghost);                       // 显示所有元素
 
 private:
-    CacheList* cache = nullptr;
-    std::unordered_map<DataType, CacheList*> map;
-    int size = 0;
-    int capacity = CACHE_CAPACITY;
-
-    CacheList* ghost = nullptr;
-    std::unordered_map<DataType, CacheList*> ghost_map;
-    int ghost_size = 0;
+    CacheList* _cache = nullptr;
+    CacheList* _ghost = nullptr;
+    uint64 _capacity = CACHE_CAPACITY;
     int ghost_capacity = CACHE_CAPACITY; // ghost链表的存储空间(固定不变的)
-
-    void Insert(CacheList* L, bool is_ghost); // 直接插入节点
-    void DeleteTail(bool is_ghost); // 删除末尾节点
-    void DetachNode(CacheList* L, bool is_ghost); // 脱离节点,即从表中删除但是不释放空间
-    int transform_time = CACHE_TRANSFROM_COUNT; // 默认从LRU到LFU转移阈值是3
-
-    /*
-    * 插入函数的要求: 如果size < capacity,直接插入
-    * 如果capacity已满，则先删除末尾的元素并将其加入ghost（单独定义加ghost函数）
-    * 然后再将元素插入本身中
-    *
-    * 此时调用加入ghost的函数,这个函数的结构和Insert相同(直接插头结点)
-    * 然后ghost如果满了则删除最后一个元素
-    */
+    uint64 _size = 0;
+    uint64 ghost_size = 0;
+    std::unordered_map<DataType, CacheList*> _map;
+    std::unordered_map<DataType, CacheList*> _ghost_map;
+    
+    void insert(CacheList* L, bool is_ghost);                    // 直接插入节点
+    void delete_tail_listnode(bool is_ghost);                    // 删除末尾节点
+    void detach_listnode(CacheList* L, bool is_ghost);           // 脱离节点,即从表中删除但是不释放空间
+    uint64 transform_time = CACHE_TRANSFROM_COUNT;                  // 默认从LRU到LFU转移阈值是3
 };
